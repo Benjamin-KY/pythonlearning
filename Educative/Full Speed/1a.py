c65fd52e324d77b248f80e3229a631be4a81c747
Benjamin Kereopa-Yorke <99622824+Benjamin-KY@users.noreply.github.com>
2022-05-08 11:45:58 +1000

    Git Push from Desktop

---
+++ Educative/Full Speed/1a.py
@@ -0,0 +1,11 @@
+a = 2
+print(type(a)) #print the data type of variable a
+
+b = 2.5
+print(type(b)) # prints data type of variable b
+
+a=2
+b=2.5
+print(a + b)  
+print ((a + b) * 2)
+print(2 + 2 + 4 - 2/3)
---
+++ Educative/Full Speed/1c.py
@@ -0,0 +1,3 @@
+def checkParity(n):
+  result = (n % 2)   ## Update result according to the parity
+  return result
